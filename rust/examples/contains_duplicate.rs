3e1db91897f7fe5ad44fb759b45679e878edf00a
Tyler Limbach <tylimbach@gmail.com>
2024-07-11 00:17:16 -0500

    rust: contains duplicate

---
+++ rust/examples/contains_duplicate.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+    for num in nums {
+        if !set.insert(num) {
+            return true; 
+        }
+    }
+    
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_1() {
+        assert_eq!(contains_duplicate(vec![1,2,3]), false);
+    }
+    
+    #[test]
+    fn test_2() {
+        assert_eq!(contains_duplicate(vec![1,2,3,3]), true);
+    }
+}
+
+fn main() {
+     
+}
