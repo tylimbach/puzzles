a423c7fa2132573d89615bc1c1861b92d0b1987f
tylimbach <tylimbach@gmail.com>
2025-07-20 15:25:56 -0500

    cpp: upgrade dev::vector to use std::ranges and memory calls

--- cpp/src/data_structures/vector.h
+++ cpp/src/data_structures/vector.h
@@ -2,15 +2,17 @@
 
 #include <cstddef>
 #include <cstring>
+#include <memory>
 #include <type_traits>
 #include <utility>
+#include <stdexcept>
 
 namespace dev {
 
-template<typename T>
+template<typename T, typename Allocator = std::allocator<T>>
 class vector {
 public:
-    vector(size_t initial_capacity = 4);
+    vector(size_t initial_capacity = 4, const Allocator& alloc = Allocator());
     ~vector();
 
     vector(const vector& other) = default; 
@@ -18,19 +20,15 @@ public:
     vector& operator=(const vector& other) = default;
     vector& operator=(vector&& other) noexcept = default;
 
-    /**
-     * @brief Remove all elements.
-     */
     void clear();
-
     template <typename... Args>
     void push_back(Args&&... args);
-    
     void reserve(size_t new_capacity);
+    
+    T& at(size_t index);
+    const T& at(size_t index) const;
 
     [[nodiscard]] bool is_full() const;
-    
-    // Additional useful methods
     [[nodiscard]] size_t size() const { return size_; }
     [[nodiscard]] size_t capacity() const { return capacity_; }
     
@@ -41,89 +39,96 @@ private:
     size_t size_;
     size_t capacity_;
     T* buf_;
+    [[no_unique_address]] Allocator alloc_;
 };
 
-// Template implementation
-
-template<typename T>
-vector<T>::vector(size_t initial_capacity)
-    : size_(0), capacity_(initial_capacity) 
+template<typename T, typename Allocator>
+vector<T, Allocator>::vector(size_t initial_capacity, const Allocator& alloc)
+    : size_(0), capacity_(initial_capacity), alloc_(alloc)
 { 
-    buf_ = static_cast<T*>(operator new(sizeof(T) * initial_capacity));
+    buf_ = std::allocator_traits<Allocator>::allocate(alloc_, initial_capacity);
 }
 
-template<typename T>
-vector<T>::~vector()
+template<typename T, typename Allocator>
+vector<T, Allocator>::~vector()
 { 
     clear();
-    operator delete(buf_);
+    std::allocator_traits<Allocator>::deallocate(alloc_, buf_, capacity_);
 }
 
-template<typename T>
-void vector<T>::clear() 
+template<typename T, typename Allocator>
+void vector<T, Allocator>::clear() 
 {
-    for (size_t i = size_; i > 0; i--) {
-        buf_[i - 1].~T();
-    }
+    std::destroy(buf_, buf_ + size_);
     size_ = 0;
 }
 
-template <typename T>
+template <typename T, typename Allocator>
 template <typename... Args>
-void vector<T>::push_back(Args&&... args) 
+void vector<T, Allocator>::push_back(Args&&... args) 
 {
     if (size_ == capacity_) {
         reserve(capacity_ * 2);
     }
 
-    new (buf_ + size_) T(std::forward<Args>(args)...);
+    std::allocator_traits<Allocator>::construct(alloc_, buf_ + size_, std::forward<Args>(args)...);
     size_++;
 }
 
-template<typename T>
-void vector<T>::reserve(size_t new_capacity) 
+template<typename T, typename Allocator>
+void vector<T, Allocator>::reserve(size_t new_capacity) 
 {
     if (new_capacity <= capacity_) {
         return;
     }
 
-    T* temp = static_cast<T*>(operator new(sizeof(T) * new_capacity));
+	[[assume(new_capacity > 0)]];
+
+    T* temp = std::allocator_traits<Allocator>::allocate(alloc_, new_capacity);
 
     if constexpr (std::is_trivially_copyable_v<T>) {
         std::memcpy(temp, buf_, size_ * sizeof(T));
     } else {
-        size_t i = 0;
         try {
-            for (; i < size_; i++) {
-                if constexpr (std::is_move_constructible_v<T>) {
-                    new (temp + i) T(std::move(buf_[i]));
-                } else {
-                    new (temp + i) T(buf_[i]);
-                }
+			auto src = std::ranges::subrange(buf_, buf_ + size_);
+			auto dest = std::ranges::subrange(temp, temp + size_);
+
+            if constexpr (std::is_move_constructible_v<T>) {
+                std::ranges::uninitialized_move(src, dest);
+            } else {
+                std::ranges::uninitialized_copy(src, dest);
             }
-        } 
-        catch (...) {
-            // Clean up any successfully constructed elements
-            for (size_t j = 0; j < i; j++) {
-                (temp + j)->~T();
-            }
-            operator delete(temp);
-            throw; // Rethrow
-        }
-        
-        // Destroy original elements
-        for (size_t i = 0; i < size_; i++) {
-            buf_[i].~T();
+            
+            std::destroy(buf_, buf_ + size_);
+        } catch (const std::exception& e) {
+            std::allocator_traits<Allocator>::deallocate(alloc_, temp, new_capacity);
+            throw;
         }
     }
 
-    operator delete(buf_);
+    std::allocator_traits<Allocator>::deallocate(alloc_, buf_, capacity_);
     buf_ = temp;
     capacity_ = new_capacity;
 }
 
-template<typename T>
-bool vector<T>::is_full() const 
+template<typename T, typename Allocator>
+T& vector<T, Allocator>::at(size_t index) {
+    if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return buf_[index];
+}
+
+template<typename T, typename Allocator>
+const T& vector<T, Allocator>::at(size_t index) const {
+    if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return buf_[index];
+}
+
+template<typename T, typename Allocator>
+bool vector<T, Allocator>::is_full() const 
 {
     return size_ == capacity_;
 }
